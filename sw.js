d6f0beb606d7274da30e867d0f83568fc01a2d91
Hl1967 <115838799+Hl1967@users.noreply.github.com>
2022-10-16 19:41:54 +0000

    🎉 INT COMMIT: 3 Updated By Github Actions

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ workbox.core.clientsClaim();
 
 // 注册成功后要立即缓存的资源列表
 // 具体缓存列表在gulpfile.js中配置，见下文
-workbox.precaching.precacheAndRoute([{"revision":"ed057baacc28a1396418834b44aacc54","url":"./index.html"},{"revision":"73e1a92da3a39646678bffb9e7817148","url":"./js/main.js"},{"revision":"7f350677d09bdc2e71438042254ecd9b","url":"./css/index.css"}], {
+workbox.precaching.precacheAndRoute([{"revision":"523d78acc06cdabd3bfb11dc01cf96a5","url":"./index.html"},{"revision":"73e1a92da3a39646678bffb9e7817148","url":"./js/main.js"},{"revision":"7f350677d09bdc2e71438042254ecd9b","url":"./css/index.css"}], {
   directoryIndex: null,
 });
 
